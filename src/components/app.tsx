22f5b5f1e38fa6e7120105524a736ef8a67de81e
Hideki Tanaka <work@tuxsnct.com>
2022-02-21 23:48:48 +0900

    initialize from template

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
-const App: React.FC = () => (
+export const App: React.FC = () => (
   <div>
     <h1>Hello World!</h1>
   </div>
 )
-
-export {
-  App
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import reactPlugin from '@vitejs/plugin-react'
 import { UserConfig } from 'vite'
 
-const config: UserConfig = {
-  base: './',
-  plugins: [reactPlugin()]
-}
-
-export default config
+export default {
+  build: {
+    outDir: '../dist'
+  },
+  plugins: [reactPlugin()],
+  root: './src'
+} as UserConfig
